eafeeddee5ece5dea0266f9a6e972cd9dedbc737
Nauman Khalid Qureshi <qureshi@arch.ethz.ch>
2024-05-15 16:31:58 +0200

    Update evoked_response_plot_000.py

--- code/processing/local/evoked_response_plot_000.py
+++ code/processing/local/evoked_response_plot_000.py
@@ -3,8 +3,8 @@ import mne
 import matplotlib.pyplot as plt
 mne.viz.set_browser_backend('matplotlib')
 
-def evoked_plot(the_data):
-    evoked = mne.read_evokeds(the_data)
+def evoked_plot(fnmae):
+    evoked = mne.read_evokeds(fname)
     colors = "blue", "red"
     title = "Evoked topoplot"
     mne.viz.plot_evoked_topo(evoked, color=colors, title=title, background_color="w", ylim=dict(eeg=[-1, 3]))
@@ -16,16 +16,3 @@ def evoked_plot(the_data):
     evoked_tar.plot_joint(title ='target evoked plot',ts_args=ts_args,topomap_args= topomap_args)
     evoked_dist.plot_joint(title ='distractors evoked plot',ts_args=ts_args,topomap_args= topomap_args)
     plt.close()
-                        #####################################################################
-                                        # SCRIPT CALL OR MODULE IMPORT #
-                        #####################################################################
-if __name__ == '__main__':
-
-
-    data_name = "04466_S021_T002"
-    the_drive = "D:"
-    the_repo = "data"
-    the_subrepo = "data_testing"
-    the_type = "04466_S021_T002"
-    the_data = f"{data_name}-ave.fif"
-    evoked_plot(the_data)
